e5d873ad047e35f0c0cd24bc989749cdbceb415f
gklelei <leleigideon97@gmail.com>
2025-12-25 22:41:55 +0300

    done

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,26 @@
-function consecutiveSubstrings(string) {
-  // type your code here
+function consecutiveSubstrings(input) {
+  const result = [];
+  const n = input.length;
+
+  if (n === 0) return [];
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j <= n; j++) {
+      result.push(input.slice(i, j));
+    }
+  }
+
+  return result;
 }
+module.exports = consecutiveSubstrings;
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
 }
 
 module.exports = consecutiveSubstrings;
-
-
